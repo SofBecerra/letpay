741a4a1edf9cc3a06a08f7be0943da21c6b7f9a9
sofia becerra <sofiabj13@hotmail.com>
2024-06-25 15:51:24 -0600

    con algunos links

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,13 +6,31 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
   end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user_id = current_user.id
+    if @event.save
+      redirect_to event_link_path(:event)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 
   def edit
   end
 
   def link
     @event = Event.find(params[:event_id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :link)
   end
 
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,9 +7,17 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    @item.update(params[:item])
+  end
 
   def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to bill_path, status: :see_other, notice: 'Item eliminado'
   end
 end
