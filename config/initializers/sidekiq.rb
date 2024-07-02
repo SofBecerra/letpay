f51423e86a2ed386bc26fdad48fff605c710f495
sofia becerra <sofiabj13@hotmail.com>
2024-07-02 13:26:41 -0600

    coment sidekinq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,6 +3,6 @@ shedule_file = 'config/schedule.yml'
 if File.exist?(shedule_file) && !File.empty?(shedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(shedule_file)
 end
-Sidekiq.configure_server do |config|
-  config.failures_max_count = 1000
-end
+# Sidekiq.configure_server do |config|
+#   config.failures_max_count = 1000
+# end
